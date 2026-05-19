03c8bd9f4e7dcdb8154923a36c345e80191d066d
Anita Balasanyan <balasanyananita@gmail.com>
2026-05-19 17:17:08 +0400

    Feat: added davaleba 4

--- ITCourse/davaleba3.cs
+++ ITCourse/davaleba3.cs
@@ -1,112 +1,109 @@
-﻿using System;
+﻿//using System;
 
-namespace ITCourse
-{
-    internal class davaleba3
-    {
-        static void Main(string[] args)
-        {
-            ////davaleba 1
-            //string username = "admin";
-            //string password = "1234";
+//namespace ITCourse
+//{
+//    internal class davaleba3
+//    {
+//        static void Main(string[] args)
+//        {
+//            ////davaleba 1
+//            //string username = "admin";
+//            //string password = "1234";
 
-            //Console.WriteLine("Enter username:");
-            //string usernameInput = Console.ReadLine();
+//            //Console.WriteLine("Enter username:");
+//            //string usernameInput = Console.ReadLine();
 
-            //Console.WriteLine("Enter password:");
-            //string passwordInput = Console.ReadLine();
+//            //Console.WriteLine("Enter password:");
+//            //string passwordInput = Console.ReadLine();
 
-            //if (username == usernameInput && password == passwordInput)
-            //{
-            //    Console.WriteLine("Welcome!");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Access Denied");
-            //}
+//            //if (username == usernameInput && password == passwordInput)
+//            //{
+//            //    Console.WriteLine("Welcome!");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine("Access Denied");
+//            //}
 
-            //davaleba 2
-            //Console.WriteLine("Enter first number:");
+//            //davaleba 2
+//            //Console.WriteLine("Enter first number:");
 
-            //if (!double.TryParse(Console.ReadLine(), out double num1))
-            //{
-            //    Console.WriteLine("Invalid first number");
-            //    return;
-            //}
+//            //if (!double.TryParse(Console.ReadLine(), out double num1))
+//            //{
+//            //    Console.WriteLine("Invalid first number");
+//            //    return;
+//            //}
 
-            //Console.WriteLine("Enter second number:");
+//            //Console.WriteLine("Enter second number:");
 
-            //if (!double.TryParse(Console.ReadLine(), out double num2))
-            //{
-            //    Console.WriteLine("Invalid second number");
-            //    return;
-            //}
+//            //if (!double.TryParse(Console.ReadLine(), out double num2))
+//            //{
+//            //    Console.WriteLine("Invalid second number");
+//            //    return;
+//            //}
 
-            //Console.WriteLine("Choose operation (+, -, *, /):");
-            //string operation = Console.ReadLine();
+//            //Console.WriteLine("Choose operation (+, -, *, /):");
+//            //string operation = Console.ReadLine();
 
-            //switch (operation)
-            //{
-            //    case "+":
-            //        Console.WriteLine($"Result: {num1 + num2}");
-            //        break;
+//            //switch (operation)
+//            //{
+//            //    case "+":
+//            //        Console.WriteLine($"Result: {num1 + num2}");
+//            //        break;
 
-            //    case "-":
-            //        Console.WriteLine($"Result: {num1 - num2}");
-            //        break;
+//            //    case "-":
+//            //        Console.WriteLine($"Result: {num1 - num2}");
+//            //        break;
 
-            //    case "*":
-            //        Console.WriteLine($"Result: {num1 * num2}");
-            //        break;
+//            //    case "*":
+//            //        Console.WriteLine($"Result: {num1 * num2}");
+//            //        break;
 
-            //    case "/":
-            //        if (num2 == 0)
-            //        {
-            //            Console.WriteLine("Cannot divide by zero");
-            //        }
-            //        else
-            //        {
-            //            Console.WriteLine($"Result: {num1 / num2}");
-            //        }
-            //        break;
+//            //    case "/":
+//            //        if (num2 == 0)
+//            //        {
+//            //            Console.WriteLine("Cannot divide by zero");
+//            //        }
+//            //        else
+//            //        {
+//            //            Console.WriteLine($"Result: {num1 / num2}");
+//            //        }
+//            //        break;
 
-            //    default:
-            //        Console.WriteLine("Invalid operation");
-            //        break;
-            //}
+//            //    default:
+//            //        Console.WriteLine("Invalid operation");
+//            //        break;
+//            //}
 
-            //davaleba 3
-            Console.WriteLine("Enter age:");
+//            //davaleba 3
+//            Console.WriteLine("Enter age:");
 
-            if (!int.TryParse(Console.ReadLine(), out int age))
-            {
-                Console.WriteLine("Invalid age");
-                return;
-            }
+//            if (!int.TryParse(Console.ReadLine(), out int age))
+//            {
+//                Console.WriteLine("Invalid age");
+//                return;
+//            }
 
-            switch (age)
-            {
-                case >= 0 and <= 12:
-                    Console.WriteLine("Child");
-                    break;
-
-                case >= 13 and <= 19:
-                    Console.WriteLine("Teenager");
-                    break;
-
-                case >= 20 and <= 64:
-                    Console.WriteLine("Adult");
-                    break;
-
-                case >= 65:
-                    Console.WriteLine("Senior");
-                    break;
-
-                default:
-                    Console.WriteLine("Invalid age");
-                    break;
-            }
-
-        }
-    }
-}
+//            if (age >= 0 && age <= 12)
+//            {
+//                Console.WriteLine("Child");
+//            }
+//            else if (age <= 19)
+//            {
+//                Console.WriteLine("Teenager");
+//            }
+//            else if (age <= 64)
+//            {
+//                Console.WriteLine("Adult");
+//            }
+//            else if (age >= 65)
+//            {
+//                Console.WriteLine("Senior");
+//            }
+//            else
+//            {
+//                Console.WriteLine("Invalid age");
+//            }
+//        }
+//    }
+//}c
---
+++ ITCourse/lecture3.cs
@@ -0,0 +1,32 @@
+﻿//using System;
+
+//namespace ITCourse
+//{
+//    internal class Lecture3
+//    {
+//        static void Main(string[] args)
+//        {
+//            string userName = "user123";
+//            string password = "pass123";
+//            string passCode = "1234";
+
+//            Console.Write("Enter username: ");
+//            string userNameInp = Console.ReadLine();
+
+//            Console.Write("Enter password: ");
+//            string userPassInp = Console.ReadLine();
+
+//            Console.Write("Enter passcode: ");
+//            string userPassCodeInp = Console.ReadLine();
+
+//            if (userName == userNameInp && (password == userPassInp || passCode == userPassCodeInp))
+//            {
+//                Console.WriteLine("Welcome");
+//            }
+//            else
+//            {
+//                Console.WriteLine("NOT Welcome");
+//            }
+//        }
+//    }
+//}
